380e784a9d7f98fa7a03186aa9ffb608234ac70d
General202 <user.den.ua@gmail.com>
2025-07-01 21:14:14 +0300

    add filter

--- booking/urls.py
+++ booking/urls.py
@@ -5,5 +5,5 @@ from booking import views
 urlpatterns = [
     path('', views.main_page, name="main"),
     path('room/<int:room_id>', views.booking_page, name='booking_page'),
-    path('', views.type_room_page, name='type_room_page')
+    path('type_room/<int:type_id>', views.type_room_page, name='type_room_page')
 ] 
